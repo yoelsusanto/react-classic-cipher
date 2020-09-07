35df23f1a689b1f2734b9f467fd9bea2bfd93039
Yoel Susanto <yoelsusantopro@yahoo.com>
2020-09-07 16:23:19 +0700

    Modify app to show algorithm page

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import Pipeline from 'pages/Pipeline';
 import AppFooter from 'components/AppFooter';
 
 import './App.css';
+import Playfair from 'pages/Playfair';
+import Affine from 'pages/Affine';
 
 const App: React.FC<{}> = () => {
     return (
         <div className="min-h-screen flex flex-col">
             <AppHeader />
             <div className="flex-1 bg-gray-300">
-                <Pipeline />
+                {/* <Pipeline /> */}
+                {/* <Playfair /> */}
+                <Affine />
             </div>
             <AppFooter />
         </div>
