9bd7af4af57ed26dbd9abb2ec6730dbe1bb4aa35
Yoel Susanto <yoelsusantopro@yahoo.com>
2020-09-09 11:45:59 +0700

    Add download button at output view

--- src/components/TextOutput/TextOutput.tsx
+++ src/components/TextOutput/TextOutput.tsx
@@ -7,10 +7,32 @@ interface ITextOutputProps {
 const TextOutput: React.FC<ITextOutputProps> = (props) => {
     const { value: displayValue } = props;
 
+    const triggerTextFileDownload = (): void => {
+        if (displayValue?.length === 0) {
+            alert('No output to be downloaded');
+            return;
+        }
+        const link = document.createElement('a');
+        link.download = 'output.txt';
+        const blob = new Blob([displayValue!], { type: 'text/plain' });
+        link.href = window.URL.createObjectURL(blob);
+        link.click();
+    };
+
     return (
         <div className="w-1/4 bg-white rounded-sm shadow-sm focus-within:shadow-outline">
             <div className="h-auto p-4 border-b">
-                <p className="text-teal-500 text-base">VIEW</p>
+                <div className="flex justify-between">
+                    <p className="text-teal-500 text-base">VIEW</p>
+                    <button
+                        className="text-teal-500 text-base outline-none"
+                        type="button"
+                        onClick={(): void => triggerTextFileDownload()}
+                    >
+                        Download
+                    </button>
+                </div>
+
                 <p className="text-teal-500 text-2xl mt-3 font-semibold">
                     Output
                 </p>
